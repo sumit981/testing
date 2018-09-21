f30f449cdab93f8858157c604b788a19be1c0645
Brst-Pc-70\Brst-pc-70 <sumit.kumar@brihaspatitech.com>
2018-09-21 12:15:08 +0530

    update index file

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ app.get('/api/cow/', cors(), async (req, res, next) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })*/
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
